2f98eeb1e4261a49cb5bc4862031457c09216b1b
ngoducthanh <ngothanh9901@gmail.com>
2023-04-01 07:48:44 +0700

    firt commit

---
+++ src/main/java/com/example/mediamarkbe/model/PlayList.java
@@ -0,0 +1,30 @@
+package com.example.mediamarkbe.model;
+
+import com.example.mediamarkbe.model.support.DateAudit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayList extends DateAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "playlist_song",
+            joinColumns = @JoinColumn(name = "playlist_id"),
+            inverseJoinColumns = @JoinColumn(name = "song_id")
+    )
+    private List<Song> songs;
+}
