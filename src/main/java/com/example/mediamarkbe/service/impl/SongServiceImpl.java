45b09cf0661e8343309f2c2b867bd049c9972896
anhemmay <69180385+anhemmay@users.noreply.github.com>
2023-05-03 19:32:11 +0700

    lấy tất cả bài hát trong album

--- src/main/java/com/example/mediamarkbe/service/impl/SongServiceImpl.java
+++ src/main/java/com/example/mediamarkbe/service/impl/SongServiceImpl.java
@@ -71,6 +71,13 @@ public class SongServiceImpl implements SongService {
         return responseObject;
     }
 
+    @Override
+    public ResponseEntity<List<SongResponse>> findSongsInAlbum(Long albumId) {
+        List<Song> songs = albumRepository.findSongsByAlbum(albumId);
+        List<SongResponse> songResponses = songs.stream().map(song -> mapSongToDto(song)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(songResponses);
+    }
+
     private SongResponse mapSongToDto(Song song){
         SongResponse songResponse = new SongResponse();
         songResponse.setId(song.getId());
@@ -78,6 +85,7 @@ public class SongServiceImpl implements SongService {
         songResponse.setDescription(song.getDescription());
         songResponse.setAvatarLink(song.getAvatarLink());
         songResponse.setMusicLink(song.getMusicLink());
+        songResponse.setCategory(song.getCategory());
         return songResponse;
     }
 }
